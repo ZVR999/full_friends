a1a6f82d4b48af2351dd8aefd0cf543d262d6868
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-17 13:42:42 -0500

    Routes, css/links/span
    Added /process route to send a query to the MySQL database to insert a new row into the friends table.
    Added simple css to center, color, and size the application.
    Now issues.
    Functionalility: Add a friend and see the reflecting table afterwards

--- server.py
+++ server.py
@@ -4,9 +4,26 @@ from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 # connect and store the connection in "mysql"; note that you pass the database name to the function
 mysql = MySQLConnector(app, 'friendsdb')
-# an example of running a query
-print mysql.query_db("SELECT * FROM friends")
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    # an example of running a query
+    friends = mysql.query_db("SELECT * FROM friends")
+    return render_template('index.html', friends=friends)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    query = 'INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) Values ( :first, :last, :occupation, now(), now())'
+    data = {
+        'first': request.form['first_name'],
+        'last': request.form['last_name'],
+        'occupation': request.form['occupation']
+    }
+    mysql.query_db(query,data)
+
+    return redirect('/')
+
 app.run(debug=True)
